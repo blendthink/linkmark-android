52e78c16b4db56a12d70dd3db5e1553b41e88188
blendthink <honwaka.developer@gmail.com>
2020-11-16 01:00:11 +0900

    Javaコマンドだけにした

--- update_api_client.sh
+++ update_api_client.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo rm -rf ./apiclient/build/openApiGenerator/linkmark
-
 java -jar openapi-generator-cli.jar generate \
   --enable-post-process-file \
   --generator-name kotlin \
@@ -13,9 +11,3 @@ java -jar openapi-generator-cli.jar generate \
   --invoker-package dev.honwakalab.linkmark.apiclient.linkmark.invoker \
   --model-package dev.honwakalab.linkmark.apiclient.linkmark.model \
   --additional-properties collectionType=list,dateLibrary=java8,enumPropertyNaming=UPPERCASE,serializationLibrary=kotlinx_serialization,useCoroutines=true
-
-sudo rm -rf ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure
-sudo rm -rf ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark
-
-sudo cp -r ./apiclient/build/openApiGenerator/linkmark/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure
-sudo cp -r ./apiclient/build/openApiGenerator/linkmark/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark
