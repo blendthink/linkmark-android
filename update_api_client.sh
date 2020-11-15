e973d8d98f54458108e48e48fdc1562ab0041c65
blendthink <honwaka.developer@gmail.com>
2020-11-16 01:09:26 +0900

    明確にカレントディレクトリを指定した

--- update_api_client.sh
+++ update_api_client.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
-rm -rf ./apiclient/build/openApiGenerator/linkmark
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+rm -rf $SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark
 
 java -jar openapi-generator-cli.jar generate \
   --generator-name kotlin \
   --library jvm-retrofit2 \
-  --output ./apiclient/build/openApiGenerator/linkmark \
-  --input-spec ./linkmark.yaml \
+  --output $SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark \
+  --input-spec $SCRIPT_DIR/linkmark.yaml \
   --package-name dev.honwakalab.linkmark.apiclient \
   --api-package dev.honwakalab.linkmark.apiclient.linkmark.api \
   --invoker-package dev.honwakalab.linkmark.apiclient.linkmark.invoker \
   --model-package dev.honwakalab.linkmark.apiclient.linkmark.model \
   --additional-properties collectionType=list,dateLibrary=java8,enumPropertyNaming=UPPERCASE,serializationLibrary=kotlinx_serialization,useCoroutines=true
 
-rm -rf ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure
-rm -rf ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark
+rm -rf $SCRIPT_DIR/apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure
+rm -rf $SCRIPT_DIR/apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark
 
-cp -r ./apiclient/build/openApiGenerator/linkmark/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure
-cp -r ./apiclient/build/openApiGenerator/linkmark/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark
+cp -r $SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure
+cp -r $SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark ./apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark
