0ec7dd7d2913cbc56e342c29744e4d7495361485
blendthink <honwaka.developer@gmail.com>
2020-11-21 18:26:56 +0900

    変数を利用してシンプルに書けるようにした

--- update_api_client.sh
+++ update_api_client.sh
@@ -1,23 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-rm -rf $SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark
+# API Client コードの出力先
+API_CLIENT_OUTPUT_DIR=$SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark
+
+API_CLIENT_PACKAGE_NAME=dev.honwakalab.linkmark.apiclient
+API_CLIENT_API_PACKAGE=dev.honwakalab.linkmark.apiclient.linkmark.api
+API_CLIENT_INVOKER_PACKAGE=dev.honwakalab.linkmark.apiclient.linkmark.invoker
+API_CLIENT_MODEL_PACKAGE=dev.honwakalab.linkmark.apiclient.linkmark.model
+
+API_CLIENT_DIR=$SCRIPT_DIR/apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient
+
+# API Client コードの出力先を削除しておく
+rm -rf $API_CLIENT_OUTPUT_DIR
 
 java -jar openapi-generator-cli.jar generate \
   --generator-name kotlin \
   --library jvm-retrofit2 \
-  --output $SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark \
+  --output $API_CLIENT_OUTPUT_DIR \
   --input-spec $SCRIPT_DIR/linkmark.yaml \
-  --package-name dev.honwakalab.linkmark.apiclient \
-  --api-package dev.honwakalab.linkmark.apiclient.linkmark.api \
-  --invoker-package dev.honwakalab.linkmark.apiclient.linkmark.invoker \
-  --model-package dev.honwakalab.linkmark.apiclient.linkmark.model \
+  --package-name $API_CLIENT_PACKAGE_NAME \
+  --api-package $API_CLIENT_API_PACKAGE \
+  --invoker-package $API_CLIENT_INVOKER_PACKAGE \
+  --model-package $API_CLIENT_MODEL_PACKAGE \
   --additional-properties collectionType=list,dateLibrary=java8,enumPropertyNaming=UPPERCASE,serializationLibrary=kotlinx_serialization,useCoroutines=true
 
-cd $SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark
+# API Client コードの出力先に移動
+cd $API_CLIENT_OUTPUT_DIR
 
-ls -R
+# コード生成に成功したらコピー先のディレクトリを削除する
+rm -rf $API_CLIENT_DIR
 
-cp -r $SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure $SCRIPT_DIR/apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure
-cp -r $SCRIPT_DIR/apiclient/build/openApiGenerator/linkmark/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark $SCRIPT_DIR/apiclient/src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark
+mkdir $API_CLIENT_DIR
+
+cp -r ./src/main/kotlin/dev/honwakalab/linkmark/apiclient/infrastructure $API_CLIENT_DIR
+cp -r ./src/main/kotlin/dev/honwakalab/linkmark/apiclient/linkmark $API_CLIENT_DIR
